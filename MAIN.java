79114b75a9c3fb1213e5518db98d231a40d47ee3
Hariprasanna Karthick <hariprasanna.karthick@gmail.com>
2026-03-22 00:23:45 +0530

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,28 @@
+package com.example.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
